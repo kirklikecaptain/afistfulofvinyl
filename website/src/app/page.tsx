268fb9f68135708b7bdf94ece2f169205e65e1a2
Kirk Hodgman <kjhodgman@gmail.com>
2023-10-11 08:54:12 -0700

    reset complete

--- website/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from "~/libs/chakra-ui/react";
-
-export default function HomePage() {
-  return (
-    <main>
-      <Heading>Home Page</Heading>
-    </main>
-  );
-}
--- website/src/app/providers.tsx
+++
@@ -1,9 +0,0 @@
-import ChakraUIProvider from "~/libs/chakra-ui/config/ThemeProvider";
-
-interface ProvidersProps {
-  children: React.ReactNode;
-}
-
-export default function Providers({ children }: ProvidersProps) {
-  return <ChakraUIProvider>{children}</ChakraUIProvider>;
-}
