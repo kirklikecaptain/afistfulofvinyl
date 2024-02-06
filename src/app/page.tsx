93592db2a4920e270acab04c06e640ff4ed2725c
Kirk Hodgman <kjhodgman@gmail.com>
2024-02-06 07:37:12 -0800

    add mantine ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ColorSchemeToggle } from "~/components/ColorSchemeToggle";
+
 export default function HomePage() {
   return (
     <main>
       <h1>Home</h1>
+      <ColorSchemeToggle />
     </main>
   );
 }
