c164f85b8c64f590d44aeb93d75787077c98c231
Kirk Hodgman <kjhodgman@gmail.com>
2024-02-10 12:31:16 -0800

    add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,11 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "A Fistful of Vinyl",
-  description: "Live sessions and interviews with independent and DIY artists from all music genres",
+  description:
+    "Live sessions and interviews with independent and DIY artists from all music genres",
 };
 
-export default function RootLayout({
-  children,
-}: React.PropsWithChildren) {
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
       <body>{children}</body>
