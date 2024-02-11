6b6814f9bcb8232d65d38ed7b4ea63ffe0d0641e
Kirk Hodgman <kjhodgman@gmail.com>
2024-02-10 21:04:10 -0800

    added Link component

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    typedRoutes: true,
     optimizePackageImports: [
       "@mantine/carousel",
       "@mantine/core",
